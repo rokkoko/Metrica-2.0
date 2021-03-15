f7e32ba3df12891e7aa4fb648fab31efe3e1e245
egor187 <py.egor.py@gmail.com>
2021-03-15 14:02:50 +0300

    views: 1) add detail_view for 'games' app with extra context ('users' and 'scores') for explore aggregation in template for user scores in game instance. 2) add list_view for 'games' app

--- games/views.py
+++ games/views.py
@@ -1,14 +1,17 @@
 import os
 import json
+from .models import Games, GameSession, GameScores
+from users.models import CustomUser
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from django.views.generic.list import ListView
+from django.views.generic import ListView, DetailView
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, UpdateView
 from games.db_actions import get_game_id_by_name, get_game_object_by_id, \
     add_game_into_db, add_game_session_into_db, add_scores
 from django.views.decorators.csrf import csrf_exempt
 from Metrica_project.stats_bot import StatsBot
+from django.db.models import Sum
 
 stats_bot_token = os.getenv("STATS_BOT_TOKEN_TEST")
 
@@ -23,3 +26,27 @@ def stats_proceed_view(request):
 
     # Бот в нашей реализации ничего не ждет от view, а лишь парсит body в json и использует его
     return HttpResponse()  # view ALWAYS must return response. В этом случае - пустой instance HttpResponse
+
+
+class GamesDetailView(DetailView):
+    model = Games
+    template_name = 'games_detail.html'
+    context_object_name = 'game'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['users'] = CustomUser.objects.distinct().filter(gamescores__game_session__game__pk=self.kwargs['pk'])
+
+        context['scores'] = dict()
+
+        # Set new pairs to context dict for template with All players for game instanse with aggregate score sum
+        for player in context['users']:
+            context['scores'].setdefault(player.username, GameScores.objects.filter(game_session__game__pk=self.kwargs['pk']).filter(user=player).aggregate(Sum('score'))['score__sum'])
+
+        return context
+
+
+class GamesListView(ListView):
+    model = Games
+    template_name = 'games_index.html'
+    context_object_name = 'games'
