54cdd0d7b7cde5d599a9a3f07218897ee713c472
egor187 <py.egor.py@gmail.com>
2021-03-18 21:38:53 +0300

    settings: connect to heroku db

--- Metrica_project/settings.py
+++ Metrica_project/settings.py
@@ -99,10 +99,10 @@ WSGI_APPLICATION = 'Metrica_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': '',
+        'NAME': os.getenv('DB_NAME_HEROKU'),
+        'USER': os.getenv('DB_USER_HEROKU'),
+        'PASSWORD': os.getenv('DB_PASSWORD_HEROKU'),
+        'HOST': os.getenv('DB_HOST_HEROKU'),
         'PORT': '5432',
     }
 }
