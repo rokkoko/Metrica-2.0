3cbcaa99c7dc2d5ab9dd436cccf2b32caf7260c0
Alexander Doroshenko <alexander.doroshenko@getpenta.com>
2021-03-21 13:42:40 +0100

    Prepare chart to be more dynamic

--- Metrica_project/static/js/charts.js
+++ Metrica_project/static/js/charts.js
@@ -1,37 +1,16 @@
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById("myChart");
+
 const myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: serverData,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+  type: "horizontalBar",
+  data: {
+    labels: serverData.labels,
+    datasets: [
+      {
+        data: serverData.data,
+      },
+    ],
+  },
+  options: {
+    legend: { display: false },
+  },
 });
