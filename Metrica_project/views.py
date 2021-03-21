0c11af28f95ccbb57577a3a9c63702e78585a3a5
Alexander Doroshenko <alexander.doroshenko@getpenta.com>
2021-03-21 11:15:49 +0100

    Add charts infrastructure

--- Metrica_project/views.py
+++ Metrica_project/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'metrica_index.html')
+    return render(request, 'metrica_index.html', context={"serverData": [12, 19, 3, 5, 2, 3]})
