44651d975c2afbec7fa7e16e9f3b65f400ae28f3
egor187 <py.egor.py@gmail.com>
2021-03-22 12:16:22 +0300

    test: add test for feedback funcionality

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.test import Client
+from users.models import CustomUser, Claim, ClaimTopic
 
-# Create your tests here.
+
+class FeedbackTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        test_user = CustomUser.objects.create(username='tester')
+        test_user.set_password('test_password')
+        self.client.login(username='tester', password='test_password')
+
+    def test_feedback(self):
+        user = CustomUser.objects.get(username='tester')
+        self.assertEqual(CustomUser.objects.get(), user)
+
+        topic = ClaimTopic.objects.create(name='first_topic')
+        self.assertEqual(ClaimTopic.objects.get(), topic)
+
+        claim = Claim.objects.create(topic=topic, claim='test_claim', claimer=user)
+        self.assertEqual(Claim.objects.get(), claim)
