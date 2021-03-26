f58d1027c03b47bd0ba7b55e0dcd088c7e08d07a
egor187 <py.egor.py@gmail.com>
2021-03-26 22:39:00 +0300

    views: set "filter" and default qs in view

--- games/views.py
+++ games/views.py
@@ -62,7 +62,15 @@ class GamesListView(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
 
         context = super().get_context_data()
-        context["filter"] = GamesFilter(self.request.GET, queryset=Games.objects.all())
+
+        if self.request.GET.get('self_game_sessions') == "on":
+            context['filter'] = GamesFilter(
+                self.request.GET,
+                queryset=GameSession.objects.filter(scores__user=self.request.user)
+            )
+        else:
+            context["filter"] = GamesFilter(
+                self.request.GET)
         return context
 
 
