a2bdf79d55b8e6005950593346acedad02655cd1
egor187 <py.egor.py@gmail.com>
2021-03-27 11:02:03 +0300

    filter: add "period" filtering

--- games/filter.py
+++ games/filter.py
@@ -8,11 +8,19 @@ class GamesFilter(django_filters.FilterSet):
     created_at = django_filters.NumberFilter(
         label='month played game',
         method='get_month',
-
     )
-    scores__user = django_filters.ModelChoiceFilter(
+    user = django_filters.ModelChoiceFilter(
         queryset=CustomUser.objects.all(),
+        field_name="scores__user",
+        label="User"
     )
+
+    period = django_filters.DateFromToRangeFilter(
+        field_name="created_at",
+        label="Period of gaming",
+        widget=django_filters.widgets.RangeWidget(attrs={'placeholder': 'e.g. 2021-04-03 (YYYY-MM-DD)'})
+    )
+
 
     def get_month(self, queryset, field_name, month):
         return queryset.filter(created_at__month=month)
@@ -22,4 +30,4 @@ class GamesFilter(django_filters.FilterSet):
 
     class Meta:
         model = GameSession
-        fields = ["game", "created_at"]
+        fields = ["game", "created_at",]
