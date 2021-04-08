e3a28d1b9ea1500a57d88e8d77532c4fdcdc9c04
egor187 <py.egor.py@gmail.com>
2021-04-08 11:11:51 +0300

    syntax fix

--- users/utils.py
+++ users/utils.py
@@ -6,8 +6,8 @@ def get_player_calendar(gs_obj):
     :param gs_obj: object of a model with game sessions of user with annotated played weekdays
     :return: dict(number of weekday : percentage of play for that day)
     """
-    list_of_played_weekdays = [elem.weekday for elem in gs_obj]
+    list_of_played_weekdays = [elem.weekday for elem in gs_obj]  # "No-query" operation. Queryset already cached in variable
     counter = Counter(list_of_played_weekdays)
-    result2 = {key: counter[key]/len(list_of_played_weekdays)*100 for key in counter.keys()}
+    result = {key: counter[key]/len(list_of_played_weekdays)*100 for key in counter.keys()}
 
-    return result2
+    return result
