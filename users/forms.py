384c449234973e5af8e90189f16b68114a672362
egor187 <py.egor.py@gmail.com>
2021-05-11 22:46:44 +0300

    feature: add new FriendshipRequestAcceptForm for "friendship" managing; refactor 'CustomUserAddFriendForm' from ModelForm to Form inheritor for union fields from two different models and further process in view;

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-import django.forms
 
-from .models import CustomUser
+from .models import CustomUser, FriendshipRequest
 from django import forms
 from .models import Claim
 
@@ -43,25 +42,35 @@ class FeedbackForm(forms.ModelForm):
             'claim': SummernoteWidget()
         }
 
+# Example how to override attributes for widgets in ModelForm (when field isn’t declared directly on the form)
+# class CustomUserAddFriendForm(forms.ModelForm):
+#     def __init__(self, *args, **kwargs):
+#         """Override to get opportunity to set css-class for ModelForm fields"""
+#         super().__init__(*args, **kwargs)
+#         self.fields['friendship'].widget.attrs.update({'class': 'special'})
+#
+#     class Meta:
+#         model = CustomUser
+#         fields = [
+#             "friendship",
+#         ]
+#         labels = {
+#             "friendship": "Select the user to whom you want to be friends",
+#         }
+#         widgets = {'friendship': django.forms.CheckboxSelectMultiple()}
 
-class CustomUserAddFriendForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        """Override to get opportunity to set css-class for ModelForm fields"""
-        super().__init__(*args, **kwargs)
-        self.fields['friendship'].widget.attrs.update({'class': 'special'})
-
-    class Meta:
-        model = CustomUser
-        fields = [
-            "friendship",
-        ]
-        labels = {
-            "friendship": "Select the user to whom you want to be friends",
-        }
-        widgets = {'friendship': django.forms.CheckboxSelectMultiple()}
+class CustomUserAddFriendForm(forms.Form):
+    """
+    Refusal from ModelForm because in form need to reference to all CustomUsers (further filtered in view)
+    and 'message' field in FriendshipRequest instance (two different models)
+    """
+    friends_candidates = forms.ModelMultipleChoiceField(
+        queryset=CustomUser.objects.all().only('friendship'),
+        widget=forms.CheckboxSelectMultiple(attrs={'class': 'special'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'class': 'special'}))
 
 
-class CustomUserRemoveFriendForm(CustomUserAddFriendForm):
+class CustomUserRemoveFriendForm(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = [
@@ -70,4 +79,16 @@ class CustomUserRemoveFriendForm(CustomUserAddFriendForm):
         labels = {
             "friendship": "Select the user('s) from whom you want to leave friends",
         }
-        widgets = {'friendship': django.forms.CheckboxSelectMultiple()}
+        widgets = {'friendship': forms.CheckboxSelectMultiple(attrs={'class': 'special'})}
+
+
+class FriendshipRequestAcceptForm(forms.ModelForm):
+    class Meta:
+        """'id' field of a models (type: AutoField) hasn't representation in forms.
+        No corresponding field type of a form. That's why it's was chosen to get form with only
+        'button' to send form with populated choice in code
+        """
+        model = FriendshipRequest
+        fields = [
+            'id',
+        ]
