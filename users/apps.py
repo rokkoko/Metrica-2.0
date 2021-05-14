ede1253839c80889bdcf6e745a839b639600501e
egor187 <py.egor.py@gmail.com>
2021-05-14 23:35:46 +0300

    feature: add "post_save" signal handler that sends an email (Mailgun) for user to whom the friend request was sent

--- users/apps.py
+++ users/apps.py
@@ -3,3 +3,20 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+    def ready(self):
+        from .signals import friendship_post_save_signal_receiver
+        from .signals import email_for_friendship_request_proccess
+        from django.db.models.signals import post_save
+        from .models import FriendshipRequest
+
+        post_save.connect(
+            friendship_post_save_signal_receiver,
+            sender=FriendshipRequest,
+            dispatch_uid='my_unique_identifier'
+        )
+        post_save.connect(
+            email_for_friendship_request_proccess,
+            sender=FriendshipRequest,
+            dispatch_uid='my_unique_identifier2'
+        )
