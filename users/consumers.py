e7f63e9413bf49e0ff2987972fac1b8b92b60ee2
egor187 <py.egor.py@gmail.com>
2021-05-18 22:20:16 +0300

    feature: add "consumer" (ASGI app) for handling websocket connection (aka Django views). Takes events (frames, messages) in scope (aka Django "request" object) and retranslate it to layers/groups between its instances (for each client).

---
+++ users/consumers.py
@@ -0,0 +1,46 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+
+
+class MyConsumer(WebsocketConsumer):
+    groups = ['friendship_messages',]
+
+    def connect(self):
+        """
+        Logic before accepting websocket connection. Last instruction must be accept()
+        """
+        self.accept()
+
+    # def disconnect(self, code):
+    #     """
+    #     Logic before disconnecting from websocket.
+    #     """
+    #     pass
+
+    def receive(self, text_data=None, bytes_data=None):
+        """
+        Receiving data (messages) from websocket
+        """
+        text_data_json = json.loads(text_data)
+        if text_data_json['message']:
+            message_json = text_data_json['message']
+            #  Send message to group
+            async_to_sync(self.channel_layer.group_send)(
+            self.groups[0],
+            {
+                'type': 'friendship_request_message',
+                'message': message_json
+            }
+        )
+
+    #  Receive message from group
+    def friendship_request_message(self, event):
+        message = event['message']
+        friendship_request_sender = message[0]["from_user__username"]
+        friendship_request_receivers = [elem['to_user'] for elem in message]
+
+        #  Send message to websocket
+        self.send(text_data=json.dumps({
+            'message': f"{friendship_request_sender} send a friendship request to {friendship_request_receivers}"
+        }))
