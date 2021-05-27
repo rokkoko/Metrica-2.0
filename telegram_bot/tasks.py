dbd3dd3cca6a54b3da9ad16f23fa4ed00eceea11
egor187 <py.egor.py@gmail.com>
2021-05-27 22:16:34 +0300

    celery: create 'task' for celery scheduler which calls service for message send to TelegramAPI by bot_token with statistics from Metrica. Telegram chat instance (address for sending) set in from db (chat_id field of a model)

---
+++ telegram_bot/tasks.py
@@ -0,0 +1,19 @@
+import django.conf
+
+from Metrica_project.celery import app
+from telegram_bot.models import Chat
+from telegram_bot.services import send_statistic_to_tg_bot
+from telegram_bot.db_actions import get_weekly_top_players_public_sessions_repr
+
+
+BOT_TOKEN = django.conf.settings.BOT_TOKEN
+CHAT_IDS_STATS_DELIVER_ORDERED = Chat.objects.filter(is_stats_deliver_ordered=True).values_list('chat_id', flat=True)
+
+
+@app.task()
+def weekly_stats_tg_notice_top_players(chats_ids=CHAT_IDS_STATS_DELIVER_ORDERED, bot_token=BOT_TOKEN):
+    """
+    Task for celery scheduler
+    """
+    stats_message = get_weekly_top_players_public_sessions_repr()
+    send_statistic_to_tg_bot(chats_ids, stats_message, bot_token)
