ef56b5c53565429e776b87d3f1127c4858ba8788
egor187 <py.egor.py@gmail.com>
2021-06-08 12:16:41 +0300

    deploy: disable ALL cache settings in prod-settings

--- Metrica_project/settings/settings_prod.py
+++ Metrica_project/settings/settings_prod.py
@@ -19,27 +19,27 @@ DATABASES = {
 # MIDDLEWARE.insert(0, 'django.middleware.cache.UpdateCacheMiddleware')
 # MIDDLEWARE.append('django.middleware.cache.FetchFromCacheMiddleware')
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-    },
-    'redis_cache': {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.getenv('REDIS_URL'),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    },
-    'db_cache': {
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'table_cache'
-        },
-    'fs_cache': {
-        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
-        'LOCATION': os.getenv('FS_CACHE_PATH')
-    }
-}
-
-CACHE_MIDDLEWARE_ALIAS = 'default'
-CACHE_MIDDLEWARE_KEY_PREFIX = ''
-CACHE_MIDDLEWARE_SECONDS = 600
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+#     },
+#     'redis_cache': {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": os.getenv('REDIS_URL'),
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     },
+#     'db_cache': {
+#         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+#         'LOCATION': 'table_cache'
+#         },
+#     'fs_cache': {
+#         'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+#         'LOCATION': os.getenv('FS_CACHE_PATH')
+#     }
+# }
+#
+# CACHE_MIDDLEWARE_ALIAS = 'default'
+# CACHE_MIDDLEWARE_KEY_PREFIX = ''
+# CACHE_MIDDLEWARE_SECONDS = 600
