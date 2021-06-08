09b334aae557390aedd1446265816fbacd9c5cac
egor187 <py.egor.py@gmail.com>
2021-06-08 17:13:33 +0300

    remove "django-heroku" lib

--- Metrica_project/settings/settings_base.py
+++ Metrica_project/settings/settings_base.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-import django_heroku
 from pathlib import Path
 import os
 
@@ -235,5 +234,3 @@ CELERY_BEAT_SCHEDULE = {
     },
 }
 CELERY_TIMEZONE = 'Europe/Moscow'
-
-django_heroku.settings(locals())
