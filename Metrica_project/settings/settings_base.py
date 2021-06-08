fd797af95915115396c175fadd7cfb95220c7ecb
egor187 <py.egor.py@gmail.com>
2021-06-08 20:07:48 +0300

    settings: add AWS custom_domain_name

--- Metrica_project/settings/settings_base.py
+++ Metrica_project/settings/settings_base.py
@@ -245,3 +245,4 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
