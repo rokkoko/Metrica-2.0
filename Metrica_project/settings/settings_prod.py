a2fa607dd0564ba6db42f432ebb3885a986666d3
egor187 <py.egor.py@gmail.com>
2021-06-09 12:12:58 +0300

    deploy: add ACL (S3) settings

--- Metrica_project/settings/settings_prod.py
+++ Metrica_project/settings/settings_prod.py
@@ -57,3 +57,4 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.eu-north-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_DEFAULT_ACL = 'public-read'
