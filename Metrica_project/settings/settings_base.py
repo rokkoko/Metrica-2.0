97351a9b94752386a9b29bcf0c8a5271908595f7
egor187 <py.egor.py@gmail.com>
2021-06-17 11:30:02 +0300

    fix: update path for media storage

--- Metrica_project/settings/settings_base.py
+++ Metrica_project/settings/settings_base.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_UPLOADS_ROOT = os.path.join(BASE_DIR, 'uploads')
+MEDIA_UPLOADS_ROOT = os.path.join(MEDIA_ROOT, 'uploads')
 
 ANYMAIL = {
     # (exact settings here depend on your ESP...)
